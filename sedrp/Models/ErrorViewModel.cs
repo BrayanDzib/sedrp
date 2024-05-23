d1d5531c783f34393511e1cc46d880c78b4ab704
BrayanDzib <20470109@campeche.tecnm.mx>
2024-05-22 21:36:37 -0600

    Initial commit

---
+++ sedrp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sedrp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
